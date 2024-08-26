e44c5b33a008a11550ba4362f4e45644f2f8b2c0
OmerAtayilmaz <omeratayilmaz@gmail.com>
2024-08-26 23:39:32 +0300

    admin prefixi kaldırıldı

--- routes/api.php
+++ routes/api.php
@@ -4,17 +4,15 @@ use App\Http\Controllers\PageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin')->group(function(){
 
-    Route::prefix('v1')->group(function(){
-        require __DIR__ .'/backoffice/v1.php';
-    });
-
-    Route::prefix('v2')->group(function(){
-        require __DIR__ .'/backoffice/v1.php';
-    });
-
+Route::prefix('v1')->group(function(){
+    require __DIR__ .'/backoffice/v1.php';
 });
+
+Route::prefix('v2')->group(function(){
+    require __DIR__ .'/backoffice/v1.php';
+});
+
 
 
 Route::get('/user', function (Request $request) {
