44bd5409fa7e8ab7992fd815129fbbf370d8fda6
OmerAtayilmaz <omeratayilmaz@gmail.com>
2024-09-22 22:24:02 +0300

    Sanctum paketi ile Login & Logout fonksiyonalitesi kazandırıldı

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserLoginRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(UserLoginRequest $request){
+
+        // Validate the password
+        if( !auth()->attempt($request->only('email','password'))){
+            return response()->json(['message'=>'Invalid Credentials'], 401);
+        }
+
+        // Generate a new token
+        $token = auth()->user()->createToken('authToken')->plainTextToken;
+
+        // Return the token
+        return response()->json([
+            'token' => $token,
+            'user' => auth()->user()
+        ]);
+       
+    }
+
+    public function logout(){
+        
+        //remove logged in user current session token
+        auth()->user()->currentAccessToken()->delete();
+    }
+}
--- routes/backoffice/v1.php
+++ routes/backoffice/v1.php
@@ -2,9 +2,20 @@
 
 use App\Http\Controllers\Api\V1\PageController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\AuthController;
+
 
 //export routes and import in api.php
 //WCMS Module
+
+Route::prefix('auth')->controller(AuthController::class)->group(function(){
+    Route::post('login','login');
+    Route::middleware('auth:sanctum')->post('logout','logout');
+    Route::post('register','register');
+    Route::post('forgot-password','forgot-password');
+    Route::post('reset-password','reset-password');
+});
+
 Route::prefix("wcms")->group(function () {
     Route::prefix("page")->controller(PageController::class)->group(function () {
         Route::get("/", "index");
